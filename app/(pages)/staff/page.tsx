8d2905395442381ae7596ef592e1fb1c3f28e25b
raphaelalingig <alingigraphaell@gmail.com>
2025-02-21 14:16:08 +0800

    loader from kent

--- app/(pages)/staff/page.tsx
+++ app/(pages)/staff/page.tsx
@@ -1,11 +1,53 @@
 import Layout from "@/app/components/Layout";
-import Navbar from "@/app/components/Navbar";
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const StaffLayout = () => {
   return (
     <Layout>
-      <div>Staff Management</div>
+      <div className="mt-12min-w-screen h-screen bg-black text-white">
+        <div className="flex items-center justify-between mb-5">
+          <h3 className="font-medium text-2xl">Staff (22)</h3>
+          <div className="flex items-center gap-4">
+            <button className="bg-[#FAC1D9] py-4 px-6 text-base font-medium rounded-md text-black">
+              Add Staff
+            </button>
+            <button className="flex items-center gap-2 bg-[#3D4142] py-4 px-6 text-base font-medium rounded-md text-white">
+              <span>Sort by</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="bg-[#FAC1D9] py-4 px-6 text-base font-medium rounded-md text-black">
+            Staff Management
+          </button>
+          <button className="bg-[#FAC1D9] py-4 px-6 text-base font-medium rounded-md text-black">
+            Attendance
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
-import Link from "next/link";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/login");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main>
-        LANDING PAGE!
-        <Link href="/login">
-          <button>Get Started</button>
-        </Link>
-      </main>
-    </div>
+    <main className="flex justify-center items-center w-full h-screen bg-[#292d2c]">
+      <div className="flex flex-row space-x-4">
+        <div className="w-12 h-12 rounded-full animate-spin border border-dashed border-cyan-500 border-t-transparent"></div>
+
+        <div className="w-12 h-12 rounded-full animate-spin border-2 border-dashed border-indigo-500 border-t-transparent"></div>
+
+        <div className="w-12 h-12 rounded-full animate-spin border-4 border-dashed border-pink-500 border-t-transparent"></div>
+
+        <div className="w-12 h-12 rounded-full animate-spin border-8 border-dashed border-green-500 border-t-transparent"></div>
+      </div>
+    </main>
   );
 }
